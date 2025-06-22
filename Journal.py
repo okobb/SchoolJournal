ff2179e6b677feef420c2246972f34ce2d8147a7
Omar Kobrosli <okobrosli2@gmail.com>
2025-06-22 20:42:41 +0300

    Added variables to answer the anaylsis questions,fixed print_journal variable names and added spaces after each section

--- Journal.py
+++ Journal.py
@@ -17,16 +17,31 @@ def build_journal (records):
             class_journal[student_name].append(student_grade)
         else:
             class_journal[student_name].append(student_grade)
+    print("\n")
 
 build_journal(records)
 
+#Function to print the journal whenever necessary.
 def print_journal(journal):
     for student in journal:
-        print (f"Student name: {student}")
-        print (f"Student grades: {journal[student]}")
-        average = round(sum(journal[student])/len(journal[student]),2)
+        student_name = student
+        student_grades = class_journal[student]
+        print (f"Student name: {student_name}")
+        print (f"Student grades: {student_grades}")
+        average = round(sum(student_grades)/len(student_grades),2)
         print (f"Student average: {average}")
-
-
+    print("\n")
 
 print_journal(class_journal)
+
+#Answering the questions:
+
+#Writing a variable for each question:
+highest_average = 0
+highest_grade = 0
+highest_student = ""
+best_performance = 0
+best_student = ""
+not_passing = []
+total_grades = 0
+overall_average = 0
